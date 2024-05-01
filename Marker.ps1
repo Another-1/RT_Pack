b8fa10b6d82b642397b382a6d0147a3f74a76a8a
Another-1 <dnp@mail.ru>
2024-05-01 14:03:41 +0300

    Remove debug stop in Marker

--- Marker.ps1
+++ Marker.ps1
@@ -35,10 +35,6 @@ $clients_torrents = Get-ClientsTorrents -clients $clients -mess_sender 'Marker' 
 $seed_cnt = 0
 $down_cnt = 0
 
-$test_torrent = @( $clients_torrents | Where-Object { $_.name -eq 'Boredoms' } ) | Select-Object -First 1
-if ( $test_torrent ) { $test_torrent }
-Pause
-
 foreach ( $torrent in $clients_torrents ) {
     if ( $torrent.state -in ( 'downloading', 'forcedDL', 'stalledDL', 'pausedDL') ) {
         if ( $torrent.tags -notlike "*$down_tag*" ) {
