d373f996bbdf214961bf193db033fe085765110b
Another-1 <dnp@mail.ru>
2024-09-11 14:52:49 +0300

    Compact logging

--- Adder.ps1
+++ Adder.ps1
@@ -287,11 +287,6 @@ if ( $nul -ne $get_blacklist -and $get_blacklist.ToUpper() -eq 'N' ) {
     Write-Log ( 'Осталось раздач: ' + $new_torrents_keys.count )
 }
 
-if ( $max_keepers -and !$kept ) {
-    Write-Log 'Указано ограничение на количество хранителей, необходимо подтянуть данные из отчётов по хранимым разделам'
-    $kept = GetRepKeptTorrents -sections $section_numbers -call_from ( $PSCommandPath | Split-Path -Leaf ).replace('.ps1', '') -max_keepers $max_keepers
-}
-
 if ( $masks_db ) {
     Write-Log 'Отфильтровываем уже известные раздачи по маскам'
     # $new_torrents_keys = $new_torrents_keys | Where-Object { !$masks_db_plain[$tracker_torrents[$_].topic_id] }
@@ -302,6 +297,17 @@ if ( $masks_db ) {
     # }
     Write-Log ( 'Осталось раздач: ' + $new_torrents_keys.count )
 }
+
+if ( $max_keepers -and !$kept ) {
+    Write-Log 'Указано ограничение на количество хранителей, необходимо подтянуть данные из отчётов по хранимым разделам'
+    $kept = GetRepKeptTorrents -sections $section_numbers -call_from ( $PSCommandPath | Split-Path -Leaf ).replace('.ps1', '') -max_keepers $max_keepers
+}
+
+Write-Log 'Отфильтровываем раздачи, у которых слишком много хранителей'
+if ( $kept ) {
+    $new_torrents_keys = $new_torrents_keys | Where-Object { $tracker_torrents[$_].topic_id -notin $kept }
+}
+Write-Log ( 'Осталось раздач: ' + $new_torrents_keys.count )
 
 $added = @{}
 $refreshed = @{}
@@ -462,10 +468,10 @@ if ( $new_torrents_keys ) {
                 # Write-Log ( 'Раздача ' + $new_tracker_data.topic_id + ' из необновляемого раздела' )
                 continue
             }
-            if ( $kept -and $new_tracker_data.topic_id -in $kept ) {
-                Write-Log "У раздачи $( $new_tracker_data.topic_id ) слишком много хранителей, пропускаем"
-                continue
-            }
+            # if ( $kept -and $new_tracker_data.topic_id -in $kept ) {
+            #     Write-Log "У раздачи $( $new_tracker_data.topic_id ) слишком много хранителей, пропускаем"
+            #     continue
+            # }
             if ( !$settings.connection.sid ) { Initialize-Forum }
             $new_torrent_file = Get-ForumTorrentFile $new_tracker_data.topic_id
             # if ( $null -eq $new_tracker_data.topic_title ) {
