80a7a696b8b3d18d5f2789f9b2187e741b98f157
Another-1 <dnp@mail.ru>
2024-09-12 08:12:35 +0300

    Add counters to Adder

--- Adder.ps1
+++ Adder.ps1
@@ -531,6 +531,9 @@ if ( $new_torrents_keys ) {
     }
 } # по наличию новых раздач.
 
+Write-Log "Добавлено: $(Get-Spell -qty $added.Count -spelling 1 -entity 'torrents' )"
+Write-Log "Обновлено: $(Get-Spell -qty $refreshed.Count -entity 'torrents' )"
+
 Remove-Variable -Name obsolete -ErrorAction SilentlyContinue
 if ( $nul -ne $tg_token -and '' -ne $tg_token -and $report_obsolete -and $report_obsolete -eq 'Y' ) {
     Write-Log 'Ищем неактуальные раздачи.'
