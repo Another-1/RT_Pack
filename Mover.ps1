7d71237856266b88a788ea054bc976bb991be9a3
Another-1 <dnp@mail.ru>
2025-07-14 17:34:49 +0300

    Fix Get-TopicIDs verbose parameter

--- Mover.ps1
+++ Mover.ps1
@@ -147,7 +147,7 @@ if ( $client.sid ) {
 
         If ( $id_subfolder.ToUpper() -eq 'Y' ) {
             Write-Log 'Получаем ID раздач из комментариев. Это может быть небыстро.'
-            Get-TopicIDs -client $client -torrent_list $torrents_list -verbose
+            Get-TopicIDs -client $client -torrent_list $torrents_list -verbos
         }
     }
     # Write-Log "Предстоит переместить $( Get-Spell -qty $torrents_list.Count -spelling 2 -entity 'torrents' )"
